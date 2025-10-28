d9bd4a761be8afc63e1554d6bbacf4870150aefd
nathanntongai <nathongai@gmail.com>
2025-10-28 18:56:47 +0300

    Feat: vite load

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  build: {
+    chyunkSizeWarningLimit: 1600,
+  },
+    server: {
     host: true, 
     allowedHosts: [
       // Allow any subdomain of loca.lt
